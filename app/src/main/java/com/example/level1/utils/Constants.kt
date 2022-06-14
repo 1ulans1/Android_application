64e4d0ee6f15e23c3460ab80b152260d4dadb578
maksimblankov <maxblankov436@gmail.com>
2022-06-14 10:48:06 +0300

    Refactor AuthActivity.kt. Crate Validator.kt, Constants.kt and Extensions.kt

---
+++ app/src/main/java/com/example/level1/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.level1.utils
+
+object Constants {
+    const val LOGIN_KEY = "LOGIN_KEY"
+    const val PASSWORD_KEY = "PASSWORD_KEY"
+    const val NAME_KEY = "PASSWORD_KEY"
+    const val REGEX_PASSWORD = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$"
+}
